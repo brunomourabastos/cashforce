3b11432a22d2948bb93eb14658a70265353a2f56
Bruno Monteiro <brunomourabastos@gmail.com>
2023-01-04 01:53:14 -0300

    feat: Preenchido migration de cnpj

--- backend/src/database/migrations/20230104044341-cnpjs.js
+++ backend/src/database/migrations/20230104044341-cnpjs.js
@@ -3,20 +3,27 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('cnpjs', {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      cnpj: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      companyType: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('cnpjs');
+
   }
 };
