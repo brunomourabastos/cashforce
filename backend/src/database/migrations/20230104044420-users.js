fcce610c2f50af68ac6249ac66aa87a7aa99ffc3
Bruno Monteiro <brunomourabastos@gmail.com>
2023-01-04 02:16:38 -0300

    feat: Preenchido migration de users

--- backend/src/database/migrations/20230104044420-users.js
+++ backend/src/database/migrations/20230104044420-users.js
@@ -3,20 +3,53 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('users', {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      mobile: { 
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      departament: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      verificationCode: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      emailChecked: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      cashforceAdm: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+        defaultValue: 0,
+      }
+    });
+    
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('users');
   }
 };
