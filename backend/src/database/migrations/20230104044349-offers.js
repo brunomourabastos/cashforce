2c1be9999793d7cb7850c1bf9089cc0533c4bf2d
Bruno Monteiro <brunomourabastos@gmail.com>
2023-01-04 01:56:16 -0300

    feat: Preenchido migration de offers

--- backend/src/database/migrations/20230104044349-offers.js
+++ backend/src/database/migrations/20230104044349-offers.js
@@ -3,20 +3,59 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('offers', {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      tax: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      tariff: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      adValorem: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      float: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      iof: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      expiresIn: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      paymentStatusSponsor: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+        defaultValue: 0
+      },
+      paymentStatusProvider: {
+        type: Sequelize.BOOLEAN,
+        allowNull: true,
+        defaultValue: 0
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      sponsorId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('offers');
   }
 };
