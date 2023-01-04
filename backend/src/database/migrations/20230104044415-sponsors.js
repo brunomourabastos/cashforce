035ada49c9a9c8d4426621de64f0b9936c2628d5
Bruno Monteiro <brunomourabastos@gmail.com>
2023-01-04 02:14:13 -0300

    feat: Preenchido migration de sponsors

--- backend/src/database/migrations/20230104044415-sponsors.js
+++ backend/src/database/migrations/20230104044415-sponsors.js
@@ -3,20 +3,113 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('sponsors', {
+      id: {
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      tradingName: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      cashforceTax: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      responsibleName: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      responsibleEmail: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      responsiblePosition: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      responsiblePhone: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      responsibleMobile: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      website: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      postalCode: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      address: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      number: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      complement: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      neighborhood: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      city: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      state: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      bank: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      bacnkAgency: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      account: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      phoneNumber: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      situation: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      situationDate: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      cnpjId: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('sponsors');
   }
 };
